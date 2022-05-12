beb4a6ea88eda8645e73171de24b9bdf71e303b8
sambernau <sambernau04@gmai.com>
2022-05-11 19:36:35 -0500

    started project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.example.workoutapp {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.workoutapp to javafx.fxml;
+    exports com.example.workoutapp;
+}
