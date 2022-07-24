57d4f942d71b0e5d177261fd505ff7f655facd5e
sambernau <sambernau04@gmai.com>
2022-07-24 14:28:05 -0500

    fixed userExists function and got rid of user builder

--- src/main/java/com/workoutapp/core/user/User.java
+++ src/main/java/com/workoutapp/core/user/User.java
@@ -1,82 +1,37 @@
 package com.workoutapp.core.user;
 
-import com.workoutapp.core.user.common.Group;
-import com.workoutapp.core.user.common.Workout;
 import com.workoutapp.core.user.database.util.Query;
-import org.w3c.dom.ls.LSOutput;
-
-import java.net.Inet4Address;
-import java.sql.ResultSet;
-import java.util.ArrayList;
 
 public class User {
-    private String email;
-    private String password;
-    private int age;
-    private double height;
-    private double weight;
+
+    //TODO: use sql builder to create new users
 
     public static void main(String[] args) {
-        userExists("Sambernau04@gmail.com");
+
     }
 
-
-    public static void userExists(String email) {
-        boolean query = Query.build()
-                .select("users")
+    public static boolean userExists(String tableName, String email) {
+        return Query.build()
+                .select(tableName)
                 .where("email", email)
                 .executeExists();
-        if (query) {
-            System.out.println("User with " + email + " already exists");
+    }
+
+    public static User attemptCreateUser(String tableName,String email) {
+        if (userExists(tableName,email)) {
+            return null;
+        } else {
+
         }
-    }
-
-    public User(String email, String password, int age, double height, double weight) {
-        this.email = email;
-        this.password = password;
-        this.age = age;
-        this.height = height;
-        this.weight = weight;
-    }
-
-
-    public String getEmail(){
-        return email;
-    }
-
-    public void setNewEmail(String newEmail){
-        email = newEmail;
-    }
-
-    public String getPassword(){
-        return password;
-    }
-    public void setPassword(String newPassword){
-        password = newPassword;
-    }
-
-    public void setAge(int newAge) {
-        age = newAge;
-    }
-
-    public double getHeight(){
-        return height;
-    }
-
-    public void setHeight(double newHeight){
-        height=newHeight;
-    }
-
-    public double getWeight(){
-        return weight;
-    }
-
-    public void setWeight(double newWeight){
-        weight=newWeight;
+            return null;
     }
 
 
 
 
 
-}
+
+
+
+
+}//End of file
---
+++ src/main/java/com/workoutapp/core/user/UserManager.java
@@ -0,0 +1,4 @@
+package com.workoutapp.core.user;
+
+public class UserManager {
+}
